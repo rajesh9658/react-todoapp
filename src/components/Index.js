be91bd041701780a57e30c089a5a37a4bc09ab17
rajesh9658 <rajeshpadhi9658@gmail.com>
2024-10-30 10:44:59 +0530

    go for it

---
+++ src/components/Index.js
@@ -0,0 +1,8 @@
+import TodoForm from "./TodoForm";
+import TodoIteams from"./TodoIteams";
+
+
+
+
+
+export {TodoForm,TodoIteams}
---
+++ src/contexts/TodoContexts.js
@@ -0,0 +1,33 @@
+import {createContext,useContext,UseContext} from "react"
+
+
+
+export const TodoContext =createContext({
+        todos:[
+            {
+                id:1,
+                todo:"todo msg",
+                completed:false,
+            }
+        ],
+        addTodo:(todo)=>{
+
+        },
+        updateTodo:(id,todo)=>{
+
+        },
+        deleteTodo:(id)=>{},
+        toggleComplete:(id)=>{}
+
+
+})
+
+
+
+
+export const useTodo =()=>{
+    return useContext(TodoContext)
+}
+
+
+export const TodoProvider =TodoContext.Provider
